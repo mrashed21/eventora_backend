eea15557639244e67ab37c44babfdf5a5640a871
mrashed21 <rashedjaman768@gmail.com>
2026-03-30 18:58:38 +0600

    add invaition

--- src/app/modules/invaitaion/invaitaion.service.ts
+++ src/app/modules/invaitaion/invaitaion.service.ts
@@ -4,11 +4,9 @@ import { prisma } from "../../lib/prisma";
 import { sendEmail } from "../../utils/email";
 
 export const invaitation_service = {
-  create: async () => {
-    //   create: async (payload: any) => {
-    // const { event_id, email } = payload;
-    const event_id = "019d3af2-2d80-778e-87c5-b6f7a8ba43f6";
-    const email = "rashedclassicit@gmail.com";
+  create: async (payload: any) => {
+    const { event_id, email } = payload;
+
     const event = await prisma.event.findUnique({
       where: {
         id: event_id,
@@ -39,5 +37,27 @@ export const invaitation_service = {
       return invitation;
     }
   },
+
+  update: async (payload: any) => {
+    const { id, status } = payload;
+    const invitation = await prisma.invitation.findUnique({
+      where: {
+        id,
+      },
+    });
+    if (!invitation) {
+      throw new api_error(status.NOT_FOUND, "Invitation not found");
+    }
+
+    const result = await prisma.invitation.update({
+      where: {
+        id,
+      },
+      data: {
+        status,
+      },
+    });
+    return result;
+  },
+  
 };
-invaitation_service.create();
