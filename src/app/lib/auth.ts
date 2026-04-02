c0e17896f383be618cb87305f5b3707bf5ef73d6
mrashed21 <rashedjaman768@gmail.com>
2026-04-02 15:45:27 +0600

    update token[A

--- src/app/lib/auth.ts
+++ src/app/lib/auth.ts
@@ -129,7 +129,7 @@ export const auth = betterAuth({
           }
         }
       },
-      expiresIn: 60 * 60, // 60 minutes in seconds
+      expiresIn: 60 * 60, // 60 minute in seconds
       otpLength: 6,
     }),
   ],
